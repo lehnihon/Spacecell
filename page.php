ba20c7bc0feb9f8b48366db97f8778513d4404dd
Leandro <lehnihon@hotmail.com>
2015-12-08 23:02:56 -0200

    incluesao de arquivos woocomerce

--- page.php
+++ page.php
@@ -12,24 +12,20 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="container" id="outros">
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php endwhile; // End of the loop. ?>		
+	</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
