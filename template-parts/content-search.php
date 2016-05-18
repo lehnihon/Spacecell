9222963918c667d620092ffc97a0001946d890de
Leandro <lehnihon@hotmail.com>
2016-05-18 17:20:27 -0300

    correçao search

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,13 +9,21 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="text-left" style="margin-bottom:25px">
+<article id="post-<?php the_ID(); ?>" class="text-left clearfix" style="margin-bottom:25px">
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 
 	</header><!-- .entry-header -->
-
+	<div class="images" style="float:left; width:10%; margin-right:50px">
+	    <?php 
+	    if (has_post_thumbnail()): 
+	    	echo the_post_thumbnail('', array('class' => "e-cinza img-responsive"));
+	    else: 
+	    	echo '<img class="img-responsive" src="'.woocommerce_placeholder_img_src().'" />';
+	    endif; 
+	    ?>
+    </div>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
